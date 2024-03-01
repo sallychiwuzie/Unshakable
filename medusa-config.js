ab2709c0c676f2fa8ca2efd66da537a177697e03
Emmanuel Alabi <alabiemmanuel177@gmail.com>
2024-03-02 00:52:50 +0100

    Updates

--- medusa-config.js
+++ medusa-config.js
@@ -46,10 +46,8 @@ const plugins = [
     resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
+      serve: true,
       autoRebuild: true,
-      develop: {
-        open: process.env.OPEN_BROWSER !== "false",
-      },
     },
   },
 ];
