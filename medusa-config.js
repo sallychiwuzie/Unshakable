e4522c97f7ff57c4e950f17851ceb96fd1292434
Emmanuel Alabi <alabiemmanuel177@gmail.com>
2024-03-02 08:49:33 +0100

    updates

--- medusa-config.js
+++ medusa-config.js
@@ -22,8 +22,8 @@ try {
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+// const ADMIN_CORS =
+//   process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
@@ -47,7 +47,7 @@ const plugins = [
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       serve: true,
-      autoRebuild: true,
+      autoRebuild: false,
     },
   },
 ];
